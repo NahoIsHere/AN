3fd7be376c34d0c2deec8c476d0a9c56074a8eeb
NahoIsHere <hugo.naho20@gmail.com>
2022-10-03 14:24:39 +0100

    Add intdectobin and Improve notes

--- horner.m
+++ horner.m
@@ -1,5 +1,7 @@
 function [px] = horner(a,x)
-%Função para desenvolver polinómios de grau x com os indices em a
+% Dado um polinómio na forma de um array "a" dos respetivos coeficientes por
+% ordem decrescente dos graus dos termos, e um ponto "x", esta função usa o 
+% método de Horner para calcular o valor px do polinómio no ponto
 n=length(a)-1;
 px=a(1);
 for i = 2:n+1
